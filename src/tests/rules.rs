03941d095a17f5624c93568ec186c69ea0c0e2e1
Julian Rosse <julian@helixbass.net>
2023-07-29 07:38:42 -0400

    test

--- src/tests/rules.rs
+++ src/tests/rules.rs
@@ -102,11 +102,6 @@ fn test_rule_options() {
 }
 
 fn no_more_than_n_uses_of_foo_rule() -> Arc<dyn Rule> {
-    // parse_options => |options| -> usize {
-    //     if options.len() != 1 {
-    //         return Err("Expected single option for # of uses");
-    //     }
-    // }
     rule! {
         name => "no_more_than_n_uses_of_foo",
         options_type => usize,
@@ -441,6 +436,79 @@ fn test_data_key_named_type() {
                             ]
                         }
                     ],
+                },
+            ]
+        },
+    );
+}
+
+#[test]
+fn test_rule_options_optional() {
+    RuleTester::run(
+        rule! {
+            name => "optional-options",
+            options_type => Option<usize>,
+            state => {
+                [per-run]
+                n: usize = options.unwrap_or(2),
+                [per-file-run]
+                num_foos: usize
+            },
+            listeners => [
+                r#"(
+                  (identifier) @c (#eq? @c "foo")
+                )"# => |node, context| {
+                    self.num_foos += 1;
+                    if self.num_foos > self.n {
+                        context.report(
+                            violation! {
+                                node => node,
+                                message => format!(r#"Can't use 'foo' more than {} times"#, self.n),
+                            }
+                        );
+                    }
+                }
+            ],
+            languages => [Rust],
+        },
+        rule_tests! {
+            valid => [
+                {
+                    code => r#"
+                        fn foo() {
+                            let foo = foo;
+                            foo();
+                            foo();
+                        }
+                    "#,
+                    options => 5,
+                },
+                r#"
+                    fn foo() {
+                        foo();
+                    }
+                "#
+            ],
+            invalid => [
+                {
+                    code => r#"
+                        fn foo() {
+                            let foo = foo;
+                            foo();
+                            foo();
+                            foo();
+                        }
+                    "#,
+                    options => 5,
+                    errors => [r#"Can't use 'foo' more than 5 times"#],
+                },
+                {
+                    code => r#"
+                        fn foo() {
+                            let foo = foo;
+                        }
+                    "#,
+                    errors => [r#"Can't use 'foo' more than 2 times"#],
                 },
             ]
         },
