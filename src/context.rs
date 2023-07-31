e722b2d10a8df3ceae765062c02553ad9f9d6699
Julian Rosse <julian@helixbass.net>
2023-07-31 13:19:23 -0400

    tokens

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,8 @@
 use std::{ops, path::Path};
 
-use tree_sitter_grep::{streaming_iterator::StreamingIterator, RopeOrSlice, SupportedLanguage};
+use tree_sitter_grep::{
+    streaming_iterator::StreamingIterator, tree_sitter::TreeCursor, RopeOrSlice, SupportedLanguage,
+};
 
 use crate::{
     rule::InstantiatedRule,
@@ -64,10 +66,7 @@ impl<'a> QueryMatchContext<'a> {
     }
 
     pub fn get_node_text(&self, node: Node) -> &'a str {
-        match &self.file_contents {
-            RopeOrSlice::Slice(file_contents) => node.utf8_text(file_contents).unwrap(),
-            RopeOrSlice::Rope(_) => unimplemented!(),
-        }
+        get_node_text(node, self.file_contents)
     }
 
     pub fn maybe_get_single_captured_node_for_query<'query, 'enclosing_node>(
@@ -141,6 +140,10 @@ impl<'a> QueryMatchContext<'a> {
             .named_children(&mut cursor)
             .any(|child| child.kind() == kind);
         ret
+    }
+
+    pub fn get_tokens(&self, node: Node<'a>) -> impl Iterator<Item = Node<'a>> {
+        get_tokens(node)
     }
 }
 
@@ -240,5 +243,171 @@ pub struct PendingFix {
 impl PendingFix {
     pub fn new(range: ops::Range<usize>, replacement: String) -> Self {
         Self { range, replacement }
+    }
+}
+
+fn get_node_text<'a>(node: Node, file_contents: RopeOrSlice<'a>) -> &'a str {
+    match file_contents {
+        RopeOrSlice::Slice(file_contents) => node.utf8_text(file_contents).unwrap(),
+        RopeOrSlice::Rope(_) => unimplemented!(),
+    }
+}
+
+macro_rules! move_to_next_sibling_or_go_to_parent_and_loop {
+    ($self:expr) => {
+        if !$self.cursor.goto_next_sibling() {
+            $self.cursor.goto_parent();
+            $self.state = JustReturnedToParent;
+            continue;
+        }
+    };
+}
+
+macro_rules! loop_if_on_comment {
+    ($self:expr) => {
+        if $self.cursor.node().kind() == "comment" {
+            $self.state = OnComment;
+            continue;
+        }
+    };
+}
+
+macro_rules! loop_landed_on_node {
+    ($self:expr) => {
+        $self.state = LandedOnNonCommentNode;
+        continue;
+    };
+}
+
+macro_rules! loop_done {
+    ($self:expr) => {
+        $self.state = Done;
+        continue;
+    };
+}
+
+fn get_tokens(node: Node) -> impl Iterator<Item = Node> {
+    TokenWalker::new(node)
+}
+
+struct TokenWalker<'a> {
+    state: TokenWalkerState,
+    cursor: TreeCursor<'a>,
+    original_node: Node<'a>,
+}
+
+impl<'a> TokenWalker<'a> {
+    pub fn new(node: Node<'a>) -> Self {
+        Self {
+            state: TokenWalkerState::Initial,
+            cursor: node.walk(),
+            original_node: node,
+        }
+    }
+}
+
+impl<'a> Iterator for TokenWalker<'a> {
+    type Item = Node<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        use TokenWalkerState::*;
+
+        loop {
+            match self.state {
+                Done => {
+                    return None;
+                }
+                Initial => {
+                    if self.cursor.node().kind() == "comment" {
+                        loop_done!(self);
+                    }
+                    if !self.cursor.goto_first_child() {
+                        self.state = Done;
+                        return Some(self.cursor.node());
+                    }
+                    loop_landed_on_node!(self);
+                }
+                ReturnedCurrentNode => {
+                    move_to_next_sibling_or_go_to_parent_and_loop!(self);
+                    loop_if_on_comment!(self);
+                    loop_landed_on_node!(self);
+                }
+                OnComment => {
+                    move_to_next_sibling_or_go_to_parent_and_loop!(self);
+                    loop_if_on_comment!(self);
+                    loop_landed_on_node!(self);
+                }
+                LandedOnNonCommentNode => {
+                    if !self.cursor.goto_first_child() {
+                        self.state = ReturnedCurrentNode;
+                        return Some(self.cursor.node());
+                    }
+                    loop_if_on_comment!(self);
+                    loop_landed_on_node!(self);
+                }
+                JustReturnedToParent => {
+                    if self.cursor.node() == self.original_node {
+                        loop_done!(self);
+                    }
+                    move_to_next_sibling_or_go_to_parent_and_loop!(self);
+                    loop_if_on_comment!(self);
+                    loop_landed_on_node!(self);
+                }
+            }
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum TokenWalkerState {
+    Initial,
+    OnComment,
+    ReturnedCurrentNode,
+    JustReturnedToParent,
+    LandedOnNonCommentNode,
+    Done,
+}
+
+#[cfg(test)]
+mod tests {
+    use tree_sitter_grep::tree_sitter::Parser;
+
+    use super::*;
+
+    fn test_all_tokens_text(text: &str, all_tokens_text: &[&str]) {
+        let mut parser = Parser::new();
+        parser
+            .set_language(SupportedLanguage::Javascript.language())
+            .unwrap();
+        let tree = parser.parse(text, None).unwrap();
+        assert_eq!(
+            get_tokens(tree.root_node())
+                .map(|node| node.utf8_text(text.as_bytes()).unwrap())
+                .collect::<Vec<_>>(),
+            all_tokens_text
+        );
+    }
+
+    #[test]
+    fn test_get_tokens_simple() {
+        test_all_tokens_text("const x = 5;", &["const", "x", "=", "5", ";"]);
+    }
+
+    #[test]
+    fn test_get_tokens_structured() {
+        test_all_tokens_text(
+            r#"
+            const whee = function(foo) {
+                for (let x = 1; x < 100; x++) {
+                    foo(x);
+                }
+            }
+        "#,
+            &[
+                "const", "whee", "=", "function", "(", "foo", ")", "{", "for", "(", "let", "x",
+                "=", "1", ";", "x", "<", "100", ";", "x", "++", ")", "{", "foo", "(", "x", ")",
+                ";", "}", "}",
+            ],
+        );
     }
 }
