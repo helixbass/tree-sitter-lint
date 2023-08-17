59247cbeb116ee695967b072a2539b413db094e6
Julian Rosse <julian@helixbass.net>
2023-08-17 09:30:25 -0400

    working on accumulated edits

--- src/fixing/accumulated_edits.rs
+++ src/fixing/accumulated_edits.rs
@@ -2,12 +2,21 @@ use std::iter::Peekable;
 
 use itertools::Itertools;
 use squalid::OptionExt;
-use tree_sitter_grep::tree_sitter::{InputEdit, Point, Range};
+use tree_sitter_grep::tree_sitter::{InputEdit, Point};
 
-#[derive(Default)]
-pub struct AccumulatedEdits(Vec<AccumulatedEdit>);
+pub struct AccumulatedEdits {
+    original_newline_offsets: Vec<usize>,
+    edits: Vec<AccumulatedEdit>,
+}
 
 impl AccumulatedEdits {
+    pub fn new(original_newline_offsets: Vec<usize>) -> Self {
+        Self {
+            original_newline_offsets,
+            edits: Default::default(),
+        }
+    }
+
     pub fn add_round_of_edits(&mut self, edits: &[(InputEdit, &str)]) {
         let mut prev_start_byte: Option<usize> = Default::default();
         for (input_edit, replacement) in edits {
@@ -18,16 +27,38 @@ impl AccumulatedEdits {
                 );
             }
 
+            self.edits.push(AccumulatedEdit {
+                original_start_byte: input_edit.start_byte,
+                original_len: input_edit.old_end_byte - input_edit.start_byte,
+                replacement_len: replacement.len(),
+                replacement_newline_offsets: get_newline_offsets(replacement).collect(),
+            });
+
             prev_start_byte = Some(input_edit.start_byte);
         }
+    }
+
+    pub fn get_input_edits(&self) -> Vec<InputEdit> {
+        self.edits
+            .iter()
+            .map(|edit| {
+                get_input_edit(
+                    edit.original_start_byte,
+                    edit.original_len,
+                    edit.replacement_len,
+                    &edit.replacement_newline_offsets,
+                    &self.original_newline_offsets,
+                )
+            })
+            .collect()
     }
 }
 
 pub struct AccumulatedEdit {
-    original_range: Range,
-    original_newline_offsets: Vec<usize>,
-    current_contents: String,
-    current_newline_offsets: Vec<usize>,
+    original_start_byte: usize,
+    original_len: usize,
+    replacement_len: usize,
+    replacement_newline_offsets: Vec<usize>,
 }
 
 fn get_point_from_newline_offsets(start_byte: usize, newline_offsets: &[usize]) -> Point {
@@ -55,13 +86,13 @@ fn get_merged_newline_offsets(
     newline_offsets: &[usize],
     start_byte: usize,
     old_end_byte: usize,
-    replacement: &str,
+    replacement_len: usize,
+    replacement_newline_offsets: &[usize],
 ) -> Vec<usize> {
     let mut newline_offsets_iter = newline_offsets.into_iter().copied().peekable();
     let mut merged_newline_offsets = Vec::with_capacity(newline_offsets.len());
     let mut has_passed_replacement = false;
-    let adjustment = (replacement.len() as isize) - (old_end_byte - start_byte) as isize;
-    let replacement_newline_offsets = get_newline_offsets(replacement).collect_vec();
+    let adjustment = (replacement_len as isize) - (old_end_byte - start_byte) as isize;
 
     fn push_all_replacement_newline_offsets(
         merged_newline_offsets: &mut Vec<usize>,
@@ -143,6 +174,63 @@ fn get_merged_newline_offsets(
     }
 }
 
+fn get_merged_newline_offsets_from_replacement(
+    newline_offsets: &[usize],
+    start_byte: usize,
+    old_end_byte: usize,
+    replacement: &str,
+) -> Vec<usize> {
+    get_merged_newline_offsets(
+        newline_offsets,
+        start_byte,
+        old_end_byte,
+        replacement.len(),
+        &get_newline_offsets(replacement).collect_vec(),
+    )
+}
+
+fn get_input_edit(
+    start_byte: usize,
+    original_len: usize,
+    replacement_len: usize,
+    replacement_newline_offsets: &[usize],
+    original_newline_offsets: &[usize],
+) -> InputEdit {
+    let old_end_byte = start_byte + original_len;
+    let new_end_byte = start_byte + replacement_len;
+    let updated_newline_offsets = get_merged_newline_offsets(
+        original_newline_offsets,
+        start_byte,
+        old_end_byte,
+        replacement_len,
+        replacement_newline_offsets,
+    );
+
+    InputEdit {
+        start_byte,
+        old_end_byte,
+        new_end_byte,
+        start_position: get_point_from_newline_offsets(start_byte, original_newline_offsets),
+        old_end_position: get_point_from_newline_offsets(old_end_byte, original_newline_offsets),
+        new_end_position: get_point_from_newline_offsets(new_end_byte, &updated_newline_offsets),
+    }
+}
+
+fn get_input_edit_from_replacement(
+    start_byte: usize,
+    original_len: usize,
+    replacement: &str,
+    original_newline_offsets: &[usize],
+) -> InputEdit {
+    get_input_edit(
+        start_byte,
+        original_len,
+        replacement.len(),
+        &get_newline_offsets(replacement).collect_vec(),
+        original_newline_offsets,
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use std::iter;
@@ -184,7 +272,7 @@ fn whee() {
     #[test]
     fn test_get_merged_newline_offsets_simple_replacement() {
         assert_eq!(
-            get_merged_newline_offsets(
+            get_merged_newline_offsets_from_replacement(
                 &get_newline_offsets(
                     r#"use foo::bar();
 fn whee() {
@@ -203,7 +291,7 @@ fn whee() {
     #[test]
     fn test_get_merged_newline_offsets_replacement_after_all_newlines() {
         assert_eq!(
-            get_merged_newline_offsets(
+            get_merged_newline_offsets_from_replacement(
                 &get_newline_offsets(
                     r#"use foo::bar();
 fn whee() {
@@ -222,7 +310,7 @@ fn whee() {
     #[test]
     fn test_get_merged_newline_offsets_replacement_before_all_newlines() {
         assert_eq!(
-            get_merged_newline_offsets(
+            get_merged_newline_offsets_from_replacement(
                 &get_newline_offsets(
                     r#"use foo::bar();
 fn whee() {
@@ -241,7 +329,7 @@ fn whee() {
     #[test]
     fn test_get_merged_newline_offsets_replacement_contains_newlines() {
         assert_eq!(
-            get_merged_newline_offsets(
+            get_merged_newline_offsets_from_replacement(
                 &get_newline_offsets(
                     r#"use foo::bar();
 fn whee() {
@@ -260,7 +348,7 @@ fn whee() {
     #[test]
     fn test_get_merged_newline_offsets_replacement_replaces_newlines() {
         assert_eq!(
-            get_merged_newline_offsets(
+            get_merged_newline_offsets_from_replacement(
                 &get_newline_offsets(
                     r#"use foo::bar();
 fn whee() {
@@ -305,35 +393,14 @@ fn whee() {
         let replacement_offset_in_chunk = shared_chunk_prefix.len();
         let replaced = &original_chunk
             [shared_chunk_prefix.len()..original_chunk.len() - shared_chunk_suffix.len()];
-        let start_byte = chunk_start_byte + replacement_offset_in_chunk;
-        let old_end_byte = start_byte + replaced.len();
-        let new_end_byte = start_byte + replacement.len();
-        let newline_offsets_in_text = get_newline_offsets(text).collect_vec();
-        let newline_offsets_in_new_text = get_merged_newline_offsets(
-            &newline_offsets_in_text,
-            start_byte,
-            old_end_byte,
-            replacement,
-        );
 
         (
-            InputEdit {
-                start_byte,
-                old_end_byte,
-                new_end_byte,
-                start_position: get_point_from_newline_offsets(
-                    start_byte,
-                    &newline_offsets_in_text,
-                ),
-                old_end_position: get_point_from_newline_offsets(
-                    old_end_byte,
-                    &newline_offsets_in_text,
-                ),
-                new_end_position: get_point_from_newline_offsets(
-                    new_end_byte,
-                    &newline_offsets_in_new_text,
-                ),
-            },
+            get_input_edit_from_replacement(
+                chunk_start_byte + replacement_offset_in_chunk,
+                replaced.len(),
+                replacement,
+                &get_newline_offsets(text).collect_vec(),
+            ),
             replacement,
         )
     }
@@ -364,21 +431,27 @@ use whee::whoa;
         );
     }
 
-    // #[test]
-    // fn test_non_overlapping_accumulation() {
-    //     let source_text = r#"use foo::bar;
-    // use bar::baz;
-    // use baz::whee;
-    // use whee::whoa;
-    // "#;
-    //     assert_eq!(
-    //         get_newline_offsets(source_text).collect_vec(),
-    //         [13, 27, 42, 58]
-    //     );
-    //     let mut accumulated_edits = AccumulatedEdits::default();
-    //     accumulated_edits.add_round_of_edits(&[
-    //         get_input_edit_and_replacement(source_text, "baz::whee", "baz::hello"),
-    //         get_input_edit_and_replacement(source_text, "foo::bar", "foo::b"),
-    //     ]);
-    // }
+    #[test]
+    fn test_single_round_of_edits() {
+        let source_text = r#"use foo::bar;
+use bar::baz;
+use baz::whee;
+use whee::whoa;
+"#;
+        let original_newline_offsets = get_newline_offsets(source_text).collect_vec();
+        assert_eq!(&original_newline_offsets, &[13, 27, 42, 58]);
+        let mut accumulated_edits = AccumulatedEdits::new(original_newline_offsets);
+        accumulated_edits.add_round_of_edits(&[
+            get_input_edit_and_replacement(source_text, "baz::whee", "baz::hello"),
+            get_input_edit_and_replacement(source_text, "foo::bar", "foo::b"),
+        ]);
+
+        assert_eq!(
+            accumulated_edits.get_input_edits(),
+            [
+                get_input_edit_and_replacement(source_text, "baz::whee", "baz::hello").0,
+                get_input_edit_and_replacement(source_text, "foo::bar", "foo::b").0,
+            ]
+        )
+    }
 }
